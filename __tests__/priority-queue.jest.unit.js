44f43a3dacd5286414ae09ab9f23d3150327a2fd
Eric Masi <emasi@vitusvet.com>
2023-11-10 12:47:03 -0500

    Make queue specific to use case

--- __tests__/priority-queue.jest.unit.js
+++ __tests__/priority-queue.jest.unit.js
@@ -3,15 +3,9 @@
 const PriorityQueue = require("../lib/priority-queue");
 
 describe("PriorityQueue behavior", () => {
-  it("should throw an error when adding an object that doesn't have the specified `priorityField`", () => {
-    const pq = new PriorityQueue("date");
-
-    expect(() => pq.enqueue({ test: true })).toThrow();
-  });
-
   it("should return elements in chronological order", () => {
     const now = new Date();
-    const pq = new PriorityQueue("date");
+    const pq = new PriorityQueue();
     const obj1 = { date: now };
     const obj2 = { date: new Date(now.getTime() + 1000) };
     const obj3 = { date: new Date(now.getTime() - 1000) };
