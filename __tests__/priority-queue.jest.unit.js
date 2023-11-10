d2642551ffaeabcd5b1ad9089b2e2a9ac8bbc64d
Eric Masi <emasi@vitusvet.com>
2023-11-10 14:15:03 -0500

    Update priority queue to allow more type control

--- __tests__/priority-queue.jest.unit.js
+++ __tests__/priority-queue.jest.unit.js
@@ -8,9 +8,9 @@ describe("PriorityQueue behavior", () => {
   it("should return elements in chronological order", () => {
     const now = new Date();
     const pq = new PriorityQueue();
-    const obj1 = { date: now };
-    const obj2 = { date: new Date(now.getTime() + 1000) };
-    const obj3 = { date: new Date(now.getTime() - 1000) };
+    const obj1 = { id: 1, date: now };
+    const obj2 = { id: 2, date: new Date(now.getTime() + 1000) };
+    const obj3 = { id: 3, date: new Date(now.getTime() - 1000) };
 
     pq.enqueue(obj1);
     pq.enqueue(obj2);
