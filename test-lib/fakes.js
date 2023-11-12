3ec75eb8deb1d9e74ce90842151a7000a5eb6ef7
Eric Masi <emasi@vitusvet.com>
2023-11-12 09:15:31 -0500

    Add test helpers

---
+++ test-lib/fakes.js
@@ -0,0 +1,60 @@
+// @ts-check
+
+function createFakeLogSource() {
+  return {
+    pop: jest.fn(),
+    popAsync: jest.fn(),
+  };
+}
+
+/**
+ * @param {number} current
+ * @param {number} max
+ * @param {number} increment
+ * @returns {() => Log | false}
+ */
+function createFakePopFn(current, max, increment, isAsync) {
+  return () => {
+    if (current > max) {
+      return false;
+    }
+
+    const log = {
+      date: new Date(current),
+      msg: `${current}`,
+    };
+
+    current += increment;
+
+    return log;
+  };
+}
+
+/**
+ * @param {number} current
+ * @param {number} max
+ * @param {number} increment
+ * @returns {() => Promise<Log | false>}
+ */
+function createFakePopAsyncFn(current, max, increment) {
+  return () => {
+    if (current > max) {
+      return new Promise((resolve) => {
+        resolve(false);
+      });
+    }
+
+    const log = {
+      date: new Date(current),
+      msg: `${current}`,
+    };
+
+    current += increment;
+
+    return new Promise((resolve) => {
+      resolve(log);
+    });
+  };
+}
+
+module.exports = { createFakeLogSource, createFakePopFn, createFakePopAsyncFn };
