8a522a257f1e3a9b23df0e40d62ac59c9a1f5a74
Eric Masi <emasi@vitusvet.com>
2023-11-12 09:46:57 -0500

    Edit test to clarify expected values

--- __tests__/batch-pop-async.jest.unit.js
+++ __tests__/batch-pop-async.jest.unit.js
@@ -1,6 +1,7 @@
 //@ts-check
 
 const { createFakePopAsyncFn } = require("../test-lib/fakes");
+const { filterValidLogs } = require("../lib/logs");
 const batchPopAsync = require("../lib/batch-pop-async");
 
 describe("batch pop async behavior", () => {
@@ -11,13 +12,21 @@ describe("batch pop async behavior", () => {
 
   it("should return logs in chronological order for 1 log source", async () => {
     const batched = batchPopAsync(createFakePopAsyncFn(1, 20, 1), 10);
-    const result = [];
+    const result = /** @type {Array<Log | false>}*/ ([]);
 
     for (let i = 0; i < 20; i++) {
       result.push(await batched());
     }
 
-    expect(result.every(Boolean)).toBe(true);
-    expect(await batched()).toBe(false);
+    const validLogs = filterValidLogs(result);
+
+    expect(validLogs).toHaveLength(20);
+
+    for (let i = 0; i < 20; i++) {
+      const log = validLogs[i];
+
+      expect(log.date.getTime()).toBe(i + 1);
+      expect(log.msg).toBe(`${i + 1}`);
+    }
   });
 });
